115575afef74169cfb20cf82253c3d37e08e63be
Afzal Jasani <afzal.jasani@fishtownanalytics.com>
2022-03-25 17:18:06 +0000

    testing sql fluff

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,7 +2,7 @@ with customers as (
 
     select 
     c_custkey as  customer_key,
-        c_name as  customer_name,
+        c_name as   customer_name,
         
         c_address as address,
         c_nationkey as nationkey,
