b188109b319e8c337fccba4ff90373471ad6ac40
Afzal Jasani <afzal.jasani@fishtownanalytics.com>
2022-03-31 19:57:08 +0000

    add new staging models

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,9 +1,8 @@
 with customers as (
 
     select 
-    c_custkey as  customer_key,
+        c_custkey as  customer_key,
         c_name as   customer_name,
-        
         c_address as address,
         c_nationkey as nationkey,
         c_phone as phone_number,
