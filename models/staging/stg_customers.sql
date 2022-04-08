d4c5cfe8426ee24e1be3ec19b530b694017b845c
Afzal Jasani <afzal.jasani@fishtownanalytics.com>
2022-04-08 20:24:29 +0000

    airtable demo

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -9,7 +9,8 @@ with customers as (
         c_mktsegment as market_segment,
         c_comment as comment
 
-    from {{ source('demo_data', 'customer') }}
+    from  {{ source('demo_data', 'customer') }}
+
 
     
 )
