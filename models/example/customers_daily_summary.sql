0a82e71bcaba7133e38759a91c6e1208ce22cd75
Afzal Jasani <afzal.jasani@fishtownanalytics.com>
2022-04-20 16:29:08 +0000

    macros and packages

--- models/example/customers_daily_summary.sql
+++ models/example/customers_daily_summary.sql
@@ -1,8 +1,10 @@
+
+
 select 
-    {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
+    {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as pk_id,
     customer_id,
     order_date,
-    count(*)
+    count(*) as total_orders
 
 from {{ ref('stg_orders') }}
 group by 1,2,3
